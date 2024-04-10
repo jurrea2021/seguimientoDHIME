e1575c372e8234dc85e849dd49fe8d897ca5b7c1
jurrea2021 <84868733+jurrea2021@users.noreply.github.com>
2024-04-10 11:35:51 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -44,7 +44,7 @@ shinyUI(fluidPage(
              br(),
              fluidRow(
                p("La herramienta propuesta facilita el análisis estadístico descriptivo por medio de gráficas, tablas y mapas para algunas de las etiquetas básicas de DHIME más representativas:
-                 PTPM_CON, PTPG_CON, TMX_CON, TMN_CON, TSSM_CON, THSM_CON, TSSM_MEDIA_D, BSHG_CON, EVTE_CON, NIVEL_H, Q_MEDIA_D, RCAM_CON, NVLM_CON, CAUDAL_H, PT_AUT_10 y NV_AUT_60.","Los datos fueron descargados de las plataformas de DHIME, con periodos de series de tiempo que van desde el año",strong(" 2013")," hasta el", strong(" 2 de abril de 2024"),". 
+                 PTPM_CON, PTPG_CON, TMX_CON, TMN_CON, TSSM_CON, THSM_CON, TSSM_MEDIA_D, BSHG_CON, EVTE_CON, NIVEL_H, Q_MEDIA_D, RCAM_CON, NVLM_CON, CAUDAL_H, PT_AUT_10 y NV_AUT_60.","Los datos fueron descargados de las plataformas de DHIME, con periodos de series de tiempo que van desde el año",strong(" 2013")," hasta el", strong(" 10 de abril de 2024"),". 
                  El Catálogo Nacional de estaciones del IDEAM utilizado en la herramienta fue descargado de la página del Instituto el ", strong("21 de noviembre de 2023"),". La herramienta se compone de 7 módulos lo cuáles se explican a continuación:",style="text-align:justify"),
                br()
                # p("La herramienta se compone de 5 módulos lo cuáles se explican a continuación:")
@@ -68,7 +68,7 @@ shinyUI(fluidPage(
                       div(actionButton('jumpToP3',strong('Avances Áreas Operativas'),style="color: white; background-color: #2C3E50; border-color: #18BC9C"),align ="center"),
                       br(),
                       p(strong("Módulo Avances Áreas Operativas: "),"A través de este módulo, se puede apreciar los avances de digitación de algunas etiquetas de DHIME por parte de las Áreas Operativas.
-                         A partir de la descarga realizada el ",strong("18 de marzo de 2024")," y la última descarga del ", strong("2 de abril de 2024")," se calculan diferencias que 
+                         A partir de la descarga realizada el ",strong("2 de abril de 2024")," y la última descarga del ", strong("9 de abril de 2024")," se calculan diferencias que 
                          permiten asociar el estado de avance por cada Área Operativa.",style="text-align:justify")
                       )
              ),
