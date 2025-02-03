c81dc1a8e647333cdefe9dbb9a3bfe71d675b452
jurrea2021 <84868733+jurrea2021@users.noreply.github.com>
2025-02-03 15:32:59 -0500

    Update functions.R

--- functions.R
+++ functions.R
@@ -21,13 +21,12 @@ filtroEtiquetasDHIME = function(id,labelId) {
 filtroAnios = function(id,labelId) {
   selectInput(id,
               label = labelId,
-              choices = c("2013" = 2013,
-                          "2014" = 2014,"2015" = 2015,
+              choices = c("2014" = 2014,"2015" = 2015,
                           "2016" = 2016,"2017" = 2017,
                           "2018" = 2018,"2019" = 2019,
                           "2020" = 2020,"2021" = 2021,
 			  "2022" = 2022,"2023" = 2023,
-			  "2024" = 2024))
+			  "2024" = 2024,"2025" = 2025))
 }
 
 filtroV1AreasOperativas = function(id,labelId) {
